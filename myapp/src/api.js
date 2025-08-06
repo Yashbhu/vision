d2628315b082fa80609db794e21d5e0b66648029
yash bahuguna <yashbahuguna918@gmail.com>
2025-08-06 16:14:46 +0530

    Update api.js

--- myapp/src/api.js
+++ myapp/src/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-   import.meta.env.VITE_APP_API_URL,
-  timeout: 30000
+  baseURL: import.meta.env.VITE_APP_API_URL,
+  timeout: 30000,
 });
 
 export default instance;
